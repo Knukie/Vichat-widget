249e745fce095e997194b0ed7ffd4d43c9f6bf50
Knukie <ianvdvalk@gmail.com>
2026-01-02 12:49:56 +0100

    Add CORS headers for widget assets

--- server.js
+++ server.js
@@ -12,26 +12,49 @@ const widgetMainPath = path.join(__dirname, 'widget', 'valki-talki-main.js');
 const widgetCssPath = path.join(__dirname, 'widget', 'valki-talki.css');
 const widgetManifestPath = path.join(__dirname, 'widget', 'valki-talki.manifest.json');
 const cspTestPath = path.join(__dirname, 'test', 'csp-test.html');
+const widgetAssetPaths = [
+  '/valki-talki.js',
+  '/valki-talki-main.js',
+  '/valki-talki.css',
+  '/valki-talki.manifest.json'
+];
+
+const applyWidgetCors = (res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+};
+
+widgetAssetPaths.forEach((route) => {
+  app.options(route, (_req, res) => {
+    applyWidgetCors(res);
+    res.sendStatus(204);
+  });
+});
 
 app.get('/valki-talki.js', (_req, res) => {
+  applyWidgetCors(res);
   res.setHeader('Content-Type', 'application/javascript; charset=utf-8');
   res.setHeader('Cache-Control', 'public, max-age=60');
   res.sendFile(widgetPath);
 });
 
 app.get('/valki-talki-main.js', (_req, res) => {
+  applyWidgetCors(res);
   res.setHeader('Content-Type', 'application/javascript; charset=utf-8');
   res.setHeader('Cache-Control', 'public, max-age=60');
   res.sendFile(widgetMainPath);
 });
 
 app.get('/valki-talki.css', (_req, res) => {
+  applyWidgetCors(res);
   res.setHeader('Content-Type', 'text/css; charset=utf-8');
   res.setHeader('Cache-Control', 'public, max-age=60');
   res.sendFile(widgetCssPath);
 });
 
 app.get('/valki-talki.manifest.json', (_req, res) => {
+  applyWidgetCors(res);
   res.setHeader('Content-Type', 'application/json; charset=utf-8');
   res.setHeader('Cache-Control', 'public, max-age=60');
   res.sendFile(widgetManifestPath);
